2c2a8d9908fc9d3899d8dabbd2bd82263b009ac5
Marius M <ulveblikk@gmail.com>
2012-05-27 11:20:47 +0200

    Removed some code.

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -18,14 +18,6 @@ namespace SportsStore.WebUI.Controllers
             repository = productRepository;
         }
 
-        //public ViewResult List(int page = 1)
-        //{
-        //    return View(repository.Products
-        //        .OrderBy(p => p.ProductID)
-        //        .Skip((page - 1) * PageSize)
-        //        .Take(PageSize));
-        //}
-
         public ViewResult List(int page = 1)
         {
             ProductsListViewModel viewModel = new ProductsListViewModel 
