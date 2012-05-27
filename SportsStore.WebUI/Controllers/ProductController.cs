5fa8f8193e4de3aadec7f121ae68162f6630bd6d
Marius M <ulveblikk@gmail.com>
2012-05-27 08:59:21 +0200

    Adding paging

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers
 {
@@ -17,12 +18,31 @@ namespace SportsStore.WebUI.Controllers
             repository = productRepository;
         }
 
+        //public ViewResult List(int page = 1)
+        //{
+        //    return View(repository.Products
+        //        .OrderBy(p => p.ProductID)
+        //        .Skip((page - 1) * PageSize)
+        //        .Take(PageSize));
+        //}
+
         public ViewResult List(int page = 1)
         {
-            return View(repository.Products
-                .OrderBy(p => p.ProductID)
-                .Skip((page - 1) * PageSize)
-                .Take(PageSize));
+            ProductsListViewModel viewModel = new ProductsListViewModel 
+            {
+                Products = repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            };
+            return View(viewModel);
+            
         }
     }
 }
